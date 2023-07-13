21626deeb8e927f7c95980dfe3db9d53fd6e835a
CJ <jayyang116@gmail.com>
2023-07-12 22:13:15 -0400

    Initial Commit

---
+++ CryptoX15/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CryptoX15
+//
+//  Created by CJ on 7/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CryptoX15/CryptoX15App.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoX15App.swift
+//  CryptoX15
+//
+//  Created by CJ on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoX15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
