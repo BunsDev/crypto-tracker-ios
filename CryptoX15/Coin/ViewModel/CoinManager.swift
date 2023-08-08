9fc1dce68973e5eed859d30ca0112f0fa0fc9486
CJ <jayyang116@gmail.com>
2023-08-07 23:31:24 -0400

    create a new variable to store error message

--- CryptoX15/Coin/ViewModel/CoinManager.swift
+++ CryptoX15/Coin/ViewModel/CoinManager.swift
@@ -8,22 +8,23 @@
 import Foundation
 import Observation
 
-@Observable
-final class CoinManager {
+@Observable final class CoinManager {
     private(set) var coins: [Coin] = []
     private let url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=true"
     private(set) var coinDic: [String: Coin] = [:]
     private(set) var top10MovingCoins: [Coin] = []
-    private(set) var error: Error? = nil
+    private(set) var errorMessage: String = ""
     
     func fetchCoinData() async {
         do {
             let data = try await APIService.downloadData(from: url)
-            coins = try JSONDecoder().decode([Coin].self, from: data)
-            coinDic = mapCoinDic()
-            top10MovingCoins = getTop10Movers()
+            try await MainActor.run {
+                coins = try JSONDecoder().decode([Coin].self, from: data)
+                top10MovingCoins = getTop10Movers()
+                coinDic = mapCoinDic()
+            }
         } catch {
-            self.error = error
+            errorMessage = error.localizedDescription
             print(error.localizedDescription)
         }
     }
