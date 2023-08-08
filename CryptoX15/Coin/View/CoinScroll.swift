31cd3e6e40a2b2befac1d1bf72d96d1a8bac6305
CJ <jayyang116@gmail.com>
2023-08-07 23:28:47 -0400

    rename the 'UserProfile' variable name

--- CryptoX15/Coin/View/CoinScroll.swift
+++ CryptoX15/Coin/View/CoinScroll.swift
@@ -9,24 +9,25 @@ import SwiftUI
 
 struct CoinScroll: View {
     let coins: [Coin]
-    @Bindable var portfolio: UserPortfolio
+    @Bindable var user: UserProfile
     
     var body: some View {
         VStack(alignment: .leading) {
             Text("Top Movers")
                 .sectionHeader()
+                .padding(.leading)
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 15) {
                     ForEach(coins) { coin in
                         NavigationLink {
-                            CoinDetailView(coin: coin, portfolio: portfolio)
+                            CoinDetailView(coin: coin, user: user)
                         } label: {
                             CoinCard(coin: coin)
                         }
                     }
                 }
+                .padding(.leading)
             }
         }
-        .padding(.leading)
     }
 }
