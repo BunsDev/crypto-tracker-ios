57f5c3a12dc1a121d4213eaedf19cc13fd02f8ab
CJ <jayyang116@gmail.com>
2023-08-07 23:52:20 -0400

    add a transition view for 'authenticating' state

---
+++ CryptoX15/Login/TransitionView.swift
@@ -0,0 +1,20 @@
+//
+//  TransitionView.swift
+//  CryptoX15
+//
+//  Created by CJ on 7/15/23.
+//
+
+import SwiftUI
+
+/// A view representing a transition screen for 'authenticating' state.
+struct TransitionView: View {
+    var body: some View {
+        Text("Cryptocoin")
+            .font(.largeTitle.bold())
+            .foregroundStyle(.whiteAndBlack)
+            .shadow(color: .whiteAndBlack, radius: 50)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.accent.gradient)
+    }
+}
